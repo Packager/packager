ac6c6658b574bdcc930d5cd7b49f65a666cf7ff3
Baryla <adrianrbarylski@gmail.com>
2020-02-16 21:42:47 +0000

    Reduced size of Packager by 4x by removing unused dep + added cjs example

--- src/loaders/utils/index.ts
+++ src/loaders/utils/index.ts
@@ -1,3 +1,2 @@
 export { default as resolver } from "./external-resolver";
 export { default as fetchNpmDependency } from "./fetch-npm-dependency";
-export { default as findDependencies } from "./find-dependencies";
