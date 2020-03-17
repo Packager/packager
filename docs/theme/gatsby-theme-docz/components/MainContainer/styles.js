3221b3dd5d3f37c11f45284e252f759725eb5e20
Baryla <adrianrbarylski@gmail.com>
2020-03-17 18:34:34 +0000

    Removed docs from the repo

--- docs/theme/gatsby-theme-docz/components/MainContainer/styles.js
+++
@@ -1,8 +0,0 @@
-import { media } from "~theme/breakpoints";
-
-export const container = {
-    backgroundColor: "background",
-    position: "relative",
-    maxWidth: 960,
-    variant: "styles.Container"
-};
